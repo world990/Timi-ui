2b060ba321f3afc2ba6c9597e2a8186756308f95
world990 <342721572@qq.com>
2020-12-24 19:39:01 +0800

    HOME页面的CSS提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <TopNav/>
+    <TopNav/>
     <div class="banner">
       <h1>轮子UI</h1>
       <h2>UI框架</h2>
@@ -13,13 +13,35 @@
 </template>
 
 <script lang="ts">
-import TopNav from '../components/TopNav.vue'
+import TopNav from '../components/TopNav.vue';
+
 export default {
   components: {TopNav}
-}
+};
 </script>
 
 <style lang="scss" scoped>
+.banner {
+  background: lightgoldenrodyellow;
+  padding: 100px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  .actions {
+    padding: 8px;
+
+    a {
+      background: #fff;
+      border-radius: 14px;
+      padding: 0 8px;
+      margin: 0 8px;
+      display: inline-block;
+      height: 28px;
+      line-height: 28px;
+    }
+  }
+}
 
 </style>
