785087cf9c1626a63099931e8ff9cc988c6d6f56
world990 <342721572@qq.com>
2020-12-24 19:47:35 +0800

    初始化Doc组件里的aside的HTML

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,24 @@
   <div>
     <TopNav/>
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="">Switch组件</router-link>
+          </li>
+          <li>
+            <router-link to="">Button组件</router-link>
+          </li>
+          <li>
+            <router-link to="">Dialog组件</router-link>
+          </li>
+          <li>
+            <router-link to="">Tabs组件</router-link>
+          </li>
+        </ol>
+
+      </aside>
       <main>主内容区</main>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>轮子UI</h1>
       <h2>UI框架</h2>
       <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
+        <a href="https://github.com/world990">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
