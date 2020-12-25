985e6d120873d4126bb239e12fed12643c7cf288
world990 <342721572@qq.com>
2020-12-25 19:14:13 +0800

    基本完成官网框架，增加一个默认页面

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import SwitchDemo from './components/SwitchDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
+import DocDemo from './components/DocDemo.vue'
 
 const history = createWebHashHistory();
  export  const router = createRouter({
@@ -12,6 +13,7 @@ const history = createWebHashHistory();
   routes: [{path: '/', component: Home},
     {
       path: '/doc', component: Doc, children: [
+        {path: '', component: DocDemo},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
