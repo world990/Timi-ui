e286b9b88aace5ce6343fa4a3e71029e269902c0
world990 <342721572@qq.com>
2020-12-26 18:10:02 +0800

    Switch CSS

---
+++ src/lib/Switch.vue
@@ -0,0 +1,31 @@
+<template>
+  <button><span></span></button>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+
+<style lang="scss" scoped>
+$h:22px;
+button{
+  height: $h;
+  width: $h*2;
+  border:none;
+  background: lightblue;
+  border-radius: $h/2;
+  position: relative;
+}
+span{
+  position: absolute;
+  top:2px;
+  left: 2px;
+  height: 18px;
+  width: 18px;
+  background: white;
+  border-radius: 9px;
+}
+
+</style>
+
