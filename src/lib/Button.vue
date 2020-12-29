3b789bf362da9b73c97d8a0fd5192babcde1b856
world990 <342721572@qq.com>
2020-12-29 17:45:16 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -52,6 +52,26 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.lunzi-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.lunzi-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
 
 </style>
